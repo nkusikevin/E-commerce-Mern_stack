f09fc4830205973ffde708cad59e86693397192f
nkusikevin <nkusikvnhart@gmail.com>
2021-01-30 15:59:55 +0200

    backend init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from "./screens/Homescreen"
+import Products from './screens/productScreen'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 function App() {
   return (
-    <>
-     <h1>Welcome to proshop</h1>
-    </>
+    <Router>
+    <Header/>
+    <main className="py-3"><Container>
+     <Route path='/' component={Home} exact/>
+     <Route path='/product/:id' component={Products}/>
+    </Container>
+    </main>
+    <Footer/>
+    </Router>
   );
 }
 
