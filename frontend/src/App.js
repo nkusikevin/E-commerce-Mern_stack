de144e1386bb35c455f8ed651a6428aa8072ca0c
nkusikevin <nkusikvnhart@gmail.com>
2021-02-07 00:42:09 +0200

    adding cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from "./screens/Homescreen"
 import Products from './screens/productScreen'
+import Cart from './screens/CartScreen'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <main className="py-3"><Container>
      <Route path='/' component={Home} exact/>
      <Route path='/product/:id' component={Products}/>
+     <Route path='/cart/:id?' component={Cart}/>
     </Container>
     </main>
     <Footer/>
